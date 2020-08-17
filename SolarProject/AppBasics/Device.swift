55e74b47c8a2438dadea34abd61522867057e29c
Yaroslav <@zhbr282@gmail.com>
2020-08-17 12:18:04 +0300

    GoogleService bug fix/ device Type structs update

--- SolarProject/AppBasics/Device.swift
+++ SolarProject/AppBasics/Device.swift
@@ -9,32 +9,35 @@
 import Foundation
 import UIKit
 
-struct ScreenSize {
-    static let SCREEN_WIDTH         = UIScreen.main.bounds.size.width
-    static let SCREEN_HEIGHT        = UIScreen.main.bounds.size.height
-    static let SCREEN_MAX_LENGTH    = max(ScreenSize.SCREEN_WIDTH, ScreenSize.SCREEN_HEIGHT)
-    static let SCREEN_MIN_LENGTH    = min(ScreenSize.SCREEN_WIDTH, ScreenSize.SCREEN_HEIGHT)
+//MARK: - ScreenSize open struct
+public struct ScreenSize {
+    static let SCREEN_WIDTH = UIScreen.main.bounds.size.width
+    static let SCREEN_HEIGHT = UIScreen.main.bounds.size.height
+    static let SCREEN_MAX_LENGTH = max(ScreenSize.SCREEN_WIDTH, ScreenSize.SCREEN_HEIGHT)
+    static let SCREEN_MIN_LENGTH = min(ScreenSize.SCREEN_WIDTH, ScreenSize.SCREEN_HEIGHT)
 }
 
-struct DeviceType {
+
+//MARK: - DeviceType open struct
+public struct DeviceType {
     
-    // iPhones
-    static let IS_IPHONE            = UIDevice.current.userInterfaceIdiom == .phone
-    static let IS_IPHONE_4_OR_LESS  = UIDevice.current.userInterfaceIdiom == .phone && ScreenSize.SCREEN_MAX_LENGTH < 568.0
-    static let IS_IPHONE_5          = UIDevice.current.userInterfaceIdiom == .phone && ScreenSize.SCREEN_MAX_LENGTH == 568.0
-    static let IS_IPHONE_6          = UIDevice.current.userInterfaceIdiom == .phone && ScreenSize.SCREEN_MAX_LENGTH == 667.0
-    static let IS_IPHONE_6P         = UIDevice.current.userInterfaceIdiom == .phone && ScreenSize.SCREEN_MAX_LENGTH == 736.0
-    static let IS_IPHONE_7          = IS_IPHONE_6
-    static let IS_IPHONE_7P         = IS_IPHONE_6P
+    //MARK: iPhones
+    static let IS_IPHONE = UIDevice.current.userInterfaceIdiom == .phone
+    static let IS_IPHONE_4_OR_LESS = UIDevice.current.userInterfaceIdiom == .phone && ScreenSize.SCREEN_MAX_LENGTH < 568.0
+    static let IS_IPHONE_5 = UIDevice.current.userInterfaceIdiom == .phone && ScreenSize.SCREEN_MAX_LENGTH == 568.0
+    static let IS_IPHONE_6 = UIDevice.current.userInterfaceIdiom == .phone && ScreenSize.SCREEN_MAX_LENGTH == 667.0
+    static let IS_IPHONE_6P = UIDevice.current.userInterfaceIdiom == .phone && ScreenSize.SCREEN_MAX_LENGTH == 736.0
+    static let IS_IPHONE_7 = IS_IPHONE_6
+    static let IS_IPHONE_7P = IS_IPHONE_6P
     
-    // iPads
-    static let IS_IPAD              = UIDevice.current.userInterfaceIdiom == .pad && ScreenSize.SCREEN_MAX_LENGTH == 1024.0
-    static let IS_IPAD_PRO_9_7      = IS_IPAD
-    static let IS_IPAD_PRO_12_9     = UIDevice.current.userInterfaceIdiom == .pad && ScreenSize.SCREEN_MAX_LENGTH == 1366.0
+    //MARK: iPads
+    static let IS_IPAD = UIDevice.current.userInterfaceIdiom == .pad && ScreenSize.SCREEN_MAX_LENGTH == 1024.0
+    static let IS_IPAD_PRO_9_7 = IS_IPAD
+    static let IS_IPAD_PRO_12_9 = UIDevice.current.userInterfaceIdiom == .pad && ScreenSize.SCREEN_MAX_LENGTH == 1366.0
     
-    // TVOS
-    static let IS_TV                = UIDevice.current.userInterfaceIdiom == .tv
+    //MARK: TVOS
+    static let IS_TV = UIDevice.current.userInterfaceIdiom == .tv
     
-    // Car Play
-    static let IS_CAR_PLAY          = UIDevice.current.userInterfaceIdiom == .carPlay
+    //MARK: Car Play
+    static let IS_CAR_PLAY = UIDevice.current.userInterfaceIdiom == .carPlay
 }
